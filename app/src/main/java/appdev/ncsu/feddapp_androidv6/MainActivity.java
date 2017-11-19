683b4f2ed3c123fe82aa8b61889846fa3b22cf7b
Shajeel_Afzal <shajeel.afzal@gmail.com>
2017-11-19 15:59:32 +0500

    Added the Signout option in the MainActivity

--- app/src/main/java/appdev/ncsu/feddapp_androidv6/MainActivity.java
+++ app/src/main/java/appdev/ncsu/feddapp_androidv6/MainActivity.java
@@ -3,6 +3,12 @@ package appdev.ncsu.feddapp_androidv6;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends BaseActivity {
 
@@ -15,5 +21,25 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_activity_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.logout) {
+            if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+                AuthUI.getInstance().signOut(this);
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
